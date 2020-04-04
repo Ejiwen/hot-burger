e258b9a59d966f33f5e6bc2da35d5a8e39549b89
Ejiwen <skibnewrim@gmail.com>
2020-04-03 17:57:38 -0700

    Updates

---
+++ precache-manifest.3a044ac612d5219bb1716abfa7f5614e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06b2046051814002300ca4f4cb9685d",
+    "url": "/hot-burger/index.html"
+  },
+  {
+    "revision": "5be1e0032a38f268d69e",
+    "url": "/hot-burger/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "124e3ebdfb3bc3fbd43a",
+    "url": "/hot-burger/static/js/2.a51759fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hot-burger/static/js/2.a51759fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be1e0032a38f268d69e",
+    "url": "/hot-burger/static/js/main.eb1664ac.chunk.js"
+  },
+  {
+    "revision": "1484f2afb9fe1dd9c77c",
+    "url": "/hot-burger/static/js/runtime-main.207dc43d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hot-burger/static/media/logo.5d5d9eef.svg"
+  }
+]);
